b54d28fbb2550c93d93db479e620fe871e91a8ed
Eddy <54838825+EddyIT99@users.noreply.github.com>
2022-01-31 21:21:11 +0100

    added the final step

--- login_linux.c
+++ login_linux.c
@@ -118,10 +118,17 @@ int main(int argc, char *argv[]) {
 
 		if (passwddata != NULL) {
 
+<<<<<<< HEAD
 			// prevent repeated online password guessing
 			if (passwddata->pwfailed >= FAIL_LIMIT){
 				printf("You've failed to login too many times\n");
 				sleep(2 + pow(2,passwddata->pwfailed - FAIL_LIMIT));
+=======
+			
+			if(passwddata->pwfailed > 2){
+			printf("You've failed to login too many times");
+			sleep(2^(passwddata->pwfailed));
+>>>>>>> 2033fc3 (added the final step)
 			}
 
 			// have successfully log in over certain times, alert user change password
@@ -170,6 +177,7 @@ int main(int argc, char *argv[]) {
 				printf(" You're in !\n");
 				//increase password age & reset fail_num
 				passwddata->pwage++;
+<<<<<<< HEAD
 				passwddata->pwfailed = 0;
 				//
 				sprintf(new_pwent, "%s:%d:%s:%s:%d:%d", passwddata->pwname, passwddata->uid, 
@@ -184,6 +192,21 @@ int main(int argc, char *argv[]) {
 				char *argVec[] = {NULL};
 				char *envVec[] = {NULL};
 				execve(cmd, argVec, envVec);
+=======
+				/*  check UID, see setuid(2) */
+				/*  start a shell, use execve(2) */
+
+				char cmd[] = "/bin/sh";
+
+				char *argVec[] = {"ls", NULL};
+				char *envVec[] = {NULL};
+
+				if (execve(cmd, argVec, envVec) == -1){
+					perror("Could not execute execve");
+				}
+			
+
+>>>>>>> 2033fc3 (added the final step)
 			}
 			else {
 				printf("Wrong Password \n");
