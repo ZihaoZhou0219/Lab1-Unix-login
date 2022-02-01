456e25b255e795eaf51a567c6fc45c5dd7b7b5e9
ZihaoZhou0219 <94221930+ZihaoZhou0219@users.noreply.github.com>
2022-02-01 10:14:52 +0100

    Update login_linux.c
    1.prevent guessing
    2.setuid to lower privilege() (remain to be improved)
    3. add comments

--- login_linux.c
+++ login_linux.c
@@ -11,8 +11,9 @@
 #include <pwd.h>
 #include <sys/types.h>
 #include <crypt.h>
-/* Uncomment next line in step 2 */
+#include <math.h>
 #include "pwent.h" 
+#include <assert.h>
 
 
 #define TRUE 1
@@ -21,6 +22,8 @@
 //newly added
 #define LINE_BUFFER_LENGTH  1000
 #define MAX_CACHE 100
+#define FAIL_LIMIT 5
+#define PASSWORD_ALERT 10
 
 // typedef struct {
 // 	char *name;
@@ -28,10 +31,7 @@
 // } replace_pos;
 
 void sighandler() {
-
-	/* add signalhandling routines here */
-	/* see 'man 2 signal' */
-	//https://en.wikipedia.org/wiki/Signal_(IPC)
+	//reference:https://en.wikipedia.org/wiki/Signal_(IPC)
 	//ctrl-C:terminate	
 	signal(SIGINT, SIG_IGN);
 	//ctrl-Z:suspend
@@ -40,11 +40,22 @@ void sighandler() {
 	signal(SIGQUIT, SIG_IGN);
 }
 
+// to align the string and fill up with spaces
+void align(char* str_align, int len){
+	int pos;
+	if (strlen(str_align) != len){
+		pos = strlen(str_align);
+		while(pos < len){
+			str_align[pos] = ' ';
+			pos;++
+		}
+		str_align[pos] = '\0';
+	}
+}
+
 
 int main(int argc, char *argv[]) {
 
-	// struct passwd *passwddata; /* this has to be redefined in step 2 */
-	/* see pwent.h */
 	mypwent *passwddata;
 
 	char important1[LENGTH] = "**IMPORTANT 1**";
@@ -53,16 +64,17 @@ int main(int argc, char *argv[]) {
 
 	char important2[LENGTH] = "**IMPORTANT 2**";
 
-	//char   *c_pass; //you might want to use this variable later...
-	char prompt[] = "password: ";
+	char prompt[] = "password: \n";
 	char *user_pass;
-	//newly added
+	// new_pwent: new string write back to database
 	char new_pwent[LINE_BUFFER_LENGTH];
+	// variables help to locate stream pointer
 	char buffer[LINE_BUFFER_LENGTH];
-	// replace_pos[MAX_CACHE];
 	char pwname_temp[LINE_BUFFER_LENGTH], passwd_temp[LINE_BUFFER_LENGTH],
 	passwd_salt_temp[LINE_BUFFER_LENGTH];
 	mypwent ent_temp = { pwname_temp, 0, passwd_temp, passwd_salt_temp, 0, 0 };
+	// choice: decide whether to login again
+	char choice[LENGTH];
 
 
 	sighandler();
@@ -78,16 +90,13 @@ int main(int argc, char *argv[]) {
 		fflush(NULL); /* Flush all  output buffers */
 		__fpurge(stdin); /* Purge any data in stdin buffer */
 
-		// if (gets(user) == NULL) /* gets() is vulnerable to buffer */
-		// 	exit(0); /*  overflow attacks.  */
+		// use fgets to prevent buffer overrun
 		if(fgets(user, sizeof(user), stdin) == NULL){
 			exit(0);
 		}
-		//******
+		//*** don't forget to add end symbol
 		user[strcspn(user, "\n")] = '\0';
 
-//		if (gets(user) == NULL) /* gets() is vulnerable to buffer */
-//			exit(0); /*  overflow attacks.  */
 
 		/* check to see if important variable is intact after input of login name - do not remove */
 		printf("Value of variable 'important 1' after input of login name: %*.*s\n",
@@ -95,28 +104,43 @@ int main(int argc, char *argv[]) {
 		printf("Value of variable 'important 2' after input of login name: %*.*s\n",
 		 		LENGTH - 1, LENGTH - 1, important2);
 
-		user_pass = getpass(prompt);
-
-
-		// printf("PW input is : %s\n", user_pass);
+		
+		// get password data according to username
 		passwddata = mygetpwnam(user);
+
+		// user doesn't exsit
 		if (passwddata == NULL){
 			printf("Can't find user!\n");
 			printf("Login Incorrect \n");
 		}
+
+
 		if (passwddata != NULL) {
-			/* You have to encrypt user_pass for this to work */
-			/* Don't forget to include the salt */	
-			printf("PW input is : %s\n", user_pass);
-			printf("PW record is : %s\n", passwddata->passwd);
-			
-			//newly added 
-			//find replace position
-			FILE *file;
-			if ((file = fopen(MYPWENT_FILENAME, "rb+")) == NULL){
-				return NULL;
+
+			// prevent repeated online password guessing
+			if (passwddata->pwfailed >= FAIL_LIMIT){
+				printf("You've failed to login too many times\n");
+				sleep(2 + pow(2,passwddata->pwfailed - FAIL_LIMIT));
 			}
 
+			// have successfully log in over certain times, alert user change password
+			if (passwddata->pwage >= PASSWORD_ALERT){
+				printf("It's better to change the password\n");
+				// printf("Do you want to change the password?(y/n)\n");
+				//to do
+
+			}
+			
+			// newly added 
+			// locate the position of the stream pointer to write new password data back to database
+			FILE *file;
+			if ((file = fopen(MYPWENT_FILENAME, "rb+")) == NULL){
+				return -1;
+			}
+
+			// line_len : accumulated offset
+			// cur_pos : current position
+			// res : position of stream pointer
 			int line_len = 0, cur_pos = 0, res;
 			while (fgets(buffer, sizeof(buffer), file) != NULL) {
 				line_len = strlen(buffer);
@@ -134,27 +158,55 @@ int main(int argc, char *argv[]) {
 					break;
 				}
 			}
+
+
+			// type in password without being echoed
+			user_pass = getpass(prompt);
+
+			// the encrypted password is identical with that in database
 			if (!strcmp(crypt(user_pass,passwddata->passwd_salt), passwddata->passwd)) {
-
+				printf(" You have failed %d times before you success\n", passwddata->pwfailed);
 				printf(" You're in !\n");
+				//increase password age & reset fail_num
 				passwddata->pwage++;
-				/*  check UID, see setuid(2) */
-				/*  start a shell, use execve(2) */
-
+				passwddata->pwfailed = 0;
+				//
+				sprintf(new_pwent, "%s:%d:%s:%s:%d:%d", passwddata->pwname, passwddata->uid, 
+				passwddata->passwd, passwddata->passwd_salt, passwddata->pwfailed, passwddata->pwage);
+				//write back to file
+				fputs(new_pwent, file);
+				fclose(file);				
+				
+				//set to normal user id to lower the priviledge
+				//**remain to change
+				setuid(1001);
+				char cmd[] = "/bin/sh";
+				char *argVec[] = {NULL};
+				char *envVec[] = {NULL};
+				execve(cmd, argVec, envVec);
 			}
 			else {
 				printf("Wrong Password \n");
 				printf("Login Incorrect \n");
-				// add fail_num and write to database
+				// wrong password and increase fail_num
 				passwddata->pwfailed++;
 			}
 			sprintf(new_pwent, "%s:%d:%s:%s:%d:%d", passwddata->pwname, passwddata->uid, 
 			passwddata->passwd, passwddata->passwd_salt, passwddata->pwfailed, passwddata->pwage);
-			printf("%s\n", new_pwent);
 			fputs(new_pwent, file);
-			// fprintf(file, "%s", new_pwent);
 			fclose(file);
 		}	
+
+		printf("Do you want to login again(y/n)?\n");
+		//type in y to continue login and otherwise quit
+		if(fgets(choice, 3, stdin) == NULL){
+			exit(0);
+		}
+		choice[1] = '\0';
+		if(strcmp(choice, "y") != 0){
+				printf("Goobye!\n");
+				return 0;
+		}
 
 	}
 	return 0;
